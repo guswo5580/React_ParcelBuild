b08b54bdc6af69646360cf2241e6bfc1d6361d4d
DESKTOP-JIU76K2\chj <guswo5580@gmail.com>
2019-03-25 23:03:31 +0900

    babel and react with parcel

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Hello');
+import React from "react";
+import ReactDOM from 'react-dom';
+import App from './src/App';
+
+ReactDOM.render(<App/>, document.getElementById("App"));
+{/* <APP/> 을 이해하기 위해서 babel을 설치해준다 */}
